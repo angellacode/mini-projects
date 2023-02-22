666add82edb4f6e1dd8b714e416bd3009f9c1e3b
Angellacode <68116342+angellacode@users.noreply.github.com>
2023-02-22 23:32:23 +0330

    Add files via upload

---
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,21 @@
+let drums=document.querySelectorAll(".drum")
+
+
+for (let i=0; i<drums.length;i++){
+    drums[i].addEventListener('click', function(){
+        console.log(this.innerHTML);
+
+        switch (key) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+        // let audio = new Audio(`./sounds/drum${i+1}.mp3`);
+        // audio.play();
+    })
+}
+
+
